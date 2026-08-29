7dccfb2d5c86708549bd3507c8f716035dd2eec6
STK <p9vcz6jmgp@privaterelay.appleid.com>
2026-08-29 20:19:52 -0300

    Update 019_company_tax_regime.sql

--- database/migrations/019_company_tax_regime.sql
+++ database/migrations/019_company_tax_regime.sql
@@ -1,9 +1,9 @@
-ALTER TABLE crm.companies
+ALTER TABLE companies
   ADD COLUMN IF NOT EXISTS tax_regime text;
 
-ALTER TABLE crm.companies
+ALTER TABLE companies
   DROP CONSTRAINT IF EXISTS companies_tax_regime_check;
 
-ALTER TABLE crm.companies
+ALTER TABLE companies
   ADD CONSTRAINT companies_tax_regime_check
-  CHECK (tax_regime IS NULL OR tax_regime IN ('simples_nacional','lucro_presumido'));
+  CHECK (tax_regime IS NULL OR tax_regime IN ('simples_nacional', 'lucro_presumido'));
