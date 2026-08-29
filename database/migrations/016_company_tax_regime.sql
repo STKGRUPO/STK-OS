140a3868f47c4f252f86595c9448f1c3d371bdaf
STK <p9vcz6jmgp@privaterelay.appleid.com>
2026-08-29 20:15:41 -0300

    Update and rename 016_company_tax_regime.sql to 019_company_tax_regime.sql

--- database/migrations/016_company_tax_regime.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE core.companies
-  ADD COLUMN IF NOT EXISTS tax_regime text
-  CHECK (tax_regime IN ('simples_nacional','lucro_presumido','lucro_real','mei'));
---
+++ database/migrations/019_company_tax_regime.sql
@@ -0,0 +1,9 @@
+ALTER TABLE crm.companies
+  ADD COLUMN IF NOT EXISTS tax_regime text;
+
+ALTER TABLE crm.companies
+  DROP CONSTRAINT IF EXISTS companies_tax_regime_check;
+
+ALTER TABLE crm.companies
+  ADD CONSTRAINT companies_tax_regime_check
+  CHECK (tax_regime IS NULL OR tax_regime IN ('simples_nacional','lucro_presumido'));
